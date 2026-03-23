b8efdb26f3b636c2e4b515b7a27431ca68f4f1ad
raikarenzoeveralenizo <your-github-email@example.com>
2026-03-23 09:28:19 +0800

    remove unwanted file

--- kumpra-backend/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import DeliveryAddressListCreateView, DeliveryAddressDetailView
-
-urlpatterns = [
-    path("addresses/", DeliveryAddressListCreateView.as_view(), name="address-list-create"),
-    path("addresses/<int:pk>/", DeliveryAddressDetailView.as_view(), name="address-detail"),
-]
